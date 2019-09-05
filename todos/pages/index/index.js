99ba8dec9f9a571485a5cf5719657539d1438eaa
chun <chunxhwan@126.com>
2019-09-05 14:11:42 +0800

    完成todos

--- todos/pages/index/index.js
+++ todos/pages/index/index.js
@@ -1,66 +1,136 @@
-// pages/index/index.js
+const util = require('../../utils/util.js')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    input: '',
+    todos: [],
+    completed: false,
+    currNum: 0,
+    logs: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  save(){
+    wx.setStorageSync('todo_list', this.data.todos)
+    wx.setStorageSync('todo_logs', this.data.logs)
   },
+  load(){
+    let todos = wx.getStorageSync('todo_list')
+    if(todos){
+      let currNum = todos.filter((item)=>{
+        return !item.completed
+      }).length
+      this.setData({
+        todos,
+        currNum
+      })
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    let logs = wx.getStorageSync('todo_logs')
+    if(logs){
+      this.setData({
+        logs
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onLoad(){
+    this.load()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  inputChangeHandle(e){
+    this.setData({
+      input: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  addLog(action, name){
+    let logs = this.data.logs
+    logs.push({
+      timestamp: util.formatTime(),
+      action,
+      name
+    })
+    this.setData({
+      logs
+    })
   },
+  addTodosHandle(){
+    if(!this.data.input || !this.data.input.trim()) return
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    let todos = this.data.todos
+    todos.push({
+      name: this.data.input,
+      completed: false
+    })
 
+    let currNum = this.data.currNum + 1
+
+    this.addLog('Add', this.data.input)
+
+    this.setData({
+      todos,
+      currNum
+    })
+
+    this.save()
   },
+  toggleTodoHandle(e){
+    let todos = this.data.todos
+    let index = e.currentTarget.dataset.index
+    todos[index].completed = !todos[index].completed
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    let currNum = this.data.currNum + ( todos[index].completed ? -1 : 1 )
 
+    this.addLog( todos[index].completed ? 'Finish' : 'Restart' , todos[index].name)
+
+    this.setData({
+      todos,
+      currNum
+    })
+
+    this.save()
   },
+  removeTodoHandle(e){
+    let index = e.currentTarget.dataset.index
+    let todos = this.data.todos
+    let item = todos.splice(index, 1)[0]
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    let currNum = this.data.currNum - ( item.completed ? 0 : 1 )
 
+    this.addLog( 'Remove' , item.name)
+
+    this.setData({
+      todos,
+      currNum
+    })
+
+    this.save()
+  },
+  togglAllHandle(){
+    this.data.completed = !this.data.completed
+    let todos = this.data.todos
+    todos.forEach((item)=>{
+      item.completed = this.data.completed
+    })
+
+    let currNum = this.data.completed ? 0 : this.data.todos.length
+
+    this.addLog( this.data.completed ? 'Finish' : 'Restart' , 'All todos')
+
+    this.setData({
+      todos,
+      currNum
+    })
+
+    this.save()
+  },
+  clearCompletedHandle(){
+    let todos = this.data.todos.filter((item)=>{
+      return !item.completed
+    })
+
+    this.addLog( 'Clear' , 'Completed todo')
+
+    this.setData({
+      todos
+    })
+
+    this.save()
   }
 })
